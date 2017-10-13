fbd2865cbb2f4194ae9aa49d08747ae621df6280
Jun Han <junhan@microsoft.com>
2017-10-13 14:13:45 +0800

    Add telemetry client

---
+++ src/common/telemetryClient.ts
@@ -0,0 +1,14 @@
+"use strict";
+import * as vscode from "vscode";
+import TelemetryReporter from "vscode-extension-telemetry";
+import { Constants } from "./constants";
+
+const extensionVersion: string = vscode.extensions.getExtension(Constants.ExtensionId).packageJSON.version;
+
+export class TelemetryClient {
+    public static sendEvent(eventName: string, properties?: { [key: string]: string; }): void {
+        this._client.sendTelemetryEvent(eventName, properties);
+    }
+
+    private static _client = new TelemetryReporter(Constants.ExtensionId, extensionVersion, Constants.AIKey);
+}

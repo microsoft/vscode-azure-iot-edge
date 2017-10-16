46714cd9ecb1bad4099bbc6514a4adc9c03d199e
Ray Fang <fangzh@microsoft.com>
2017-10-16 20:26:05 +0800

    Extract getDockerfilePath method

--- src/container/containerManager.ts
+++ src/container/containerManager.ts
@@ -18,25 +18,14 @@ export class ContainerManager {
     }
 
     public async buildDockerImage(dockerfileFromContext?: vscode.Uri): Promise<string> {
-        let dockerfileItem: vscode.QuickPickItem;
-        if (dockerfileFromContext) {
-            dockerfileItem = this.getDockerfileItem(dockerfileFromContext);
-        } else {
-            const dockerfileList: vscode.Uri[] = await this.getDockerfileList();
-            if (!dockerfileList || dockerfileList.length === 0) {
-                vscode.window.showErrorMessage("No Dockerfile can be found under this workspace.");
-                return;
-            }
+        const dockerfilePath: string = await this.getDockerfilePath(dockerfileFromContext);
 
-            const dockerfileItemList: vscode.QuickPickItem[] = this.getDockerfileItemList(dockerfileList);
-            dockerfileItem = await vscode.window.showQuickPick(dockerfileItemList, { placeHolder: "Select Dockerfile" });
-        }
-        if (dockerfileItem) {
+        if (dockerfilePath) {
             const buildArguments: string = await vscode.window.showInputBox({ prompt: "Add build arguments", placeHolder: "e.g., EXE_DIR=./bin/Release/netcoreapp2.0/publish", ignoreFocusOut: true });
             if (buildArguments !== undefined) { // continue if users don't press esc, but accept empty strings
                 const imageName: string = await vscode.window.showInputBox({ prompt: "Enter image name", placeHolder: "e.g., myregistry.azurecr.io/myedgemodule:latest", ignoreFocusOut: true });
                 if (imageName !== undefined) {  // continue if users don't press esc, but accept empty strings
-                    Executor.runInTerminal(`docker build -f ${dockerfileItem.detail} --build-arg ${buildArguments} -t ${imageName} ${vscode.workspace.rootPath}`);
+                    Executor.runInTerminal(`docker build -f ${dockerfilePath} --build-arg ${buildArguments} -t ${imageName} ${vscode.workspace.rootPath}`);
                     TelemetryClient.sendEvent("end-build-docker-image");
 
                     // debug only
@@ -53,6 +42,25 @@ export class ContainerManager {
     public async pushDockerImage(imageName: string) {
         Executor.runInTerminal(`docker push ${imageName}`);
         TelemetryClient.sendEvent("end-push-docker-image");
+    }
+
+    private async getDockerfilePath(dockerfileFromContext?: vscode.Uri): Promise<string> {
+        if (dockerfileFromContext) {
+            return dockerfileFromContext.fsPath;
+        } else {
+            const dockerfileList: vscode.Uri[] = await this.getDockerfileList();
+            if (!dockerfileList || dockerfileList.length === 0) {
+                vscode.window.showErrorMessage("No Dockerfile can be found under this workspace.");
+                return;
+            }
+
+            const dockerfileItemList: vscode.QuickPickItem[] = this.getDockerfileItemList(dockerfileList);
+            const dockerfileItem: vscode.QuickPickItem = await vscode.window.showQuickPick(dockerfileItemList, { placeHolder: "Select Dockerfile" });
+
+            if (dockerfileItem) {
+                return dockerfileItem.detail;
+            }
+        }
     }
 
     private async getDockerfileList(): Promise<vscode.Uri[]> {

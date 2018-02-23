368ddc5ad08dcea4ba7bbc50acbeb7bdfd206876
Jun Han <junhan@microsoft.com>
2018-02-23 16:05:42 +0800

    Code cleanup

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -3,9 +3,7 @@
 export class Constants {
     public static ExtensionId = "vsciot-vscode.azure-iot-edge";
     public static AIKey = "fed7fc65-5b4a-4e66-9d46-c5f016d4e2b4";
-    public static IotHubConnectionStringKey = "iotHubConnectionString";
     public static deploymentFile = "deployment.json";
-    public static routesFile = "routes.json";
     public static launchFile = "launch.json";
     public static dockerfileNamePattern = "**/[dD]ocker[fF]ile*";
     public static dotNetProjectFileNamePattern = "**/*.{csproj,fsproj}";
--- src/common/utility.ts
+++ src/common/utility.ts
@@ -1,5 +1,4 @@
 "use strict";
-import * as iothub from "azure-iothub";
 import * as fse from "fs-extra";
 import * as os from "os";
 import * as path from "path";
@@ -12,42 +11,6 @@ import { UserCancelledError } from "./UserCancelledError";
 export class Utility {
     public static getConfiguration(): vscode.WorkspaceConfiguration {
         return vscode.workspace.getConfiguration("azure-iot-edge");
-    }
-
-    public static getDeviceIdFromConnectionString(deviceConnectionString: string): string {
-        const result = /DeviceId=([^=;]+);/.exec(deviceConnectionString);
-        return result ? result[1] : "";
-    }
-
-    public static getHostNameFromConnectionString(iotHubConnectionString: string): string {
-        const result = /^HostName=([^=]+);/.exec(iotHubConnectionString);
-        return result ? result[1] : "";
-    }
-
-    public static showfile(fileName: string) {
-        vscode.workspace.openTextDocument(fileName).then((document: vscode.TextDocument) => {
-            vscode.window.showTextDocument(document);
-        });
-    }
-
-    public static getDevices(): Promise<iothub.Device[]> {
-        const config = Utility.getConfiguration();
-        const iotHubConnectionString = config.get<string>(Constants.IotHubConnectionStringKey);
-        if (!iotHubConnectionString) {
-            vscode.window.showWarningMessage("Please login first.");
-            return;
-        }
-        const registry = iothub.Registry.fromConnectionString(iotHubConnectionString);
-
-        return new Promise<iothub.Device[]>((resolve, reject) => {
-            registry.list((err, deviceList) => {
-                if (err) {
-                    reject(`[Failed to get IoT Hub devices] ${err.message}`);
-                } else {
-                    resolve(deviceList);
-                }
-            });
-        });
     }
 
     public static checkWorkspace(): boolean {

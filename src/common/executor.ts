c0bd54dfc5356db1a7b91fb690f64069206fe96f
Xin Shi <xshi@microsoft.com>
2018-03-12 23:41:01 +0800

    Update executor.ts

--- src/common/executor.ts
+++ src/common/executor.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
-"use strict"; 
+"use strict";
 import { ChildProcess, exec, execSync, spawn, SpawnOptions } from "child_process";
 import * as vscode from "vscode";
 

4a6e17e0a2a2fd2dc7600ebbf639f8e8278a2ae3
Jun Han <formulahendry@gmail.com>
2018-08-27 14:12:34 +0800

    Change back to master branch for Python (#238)
    * Change back to master branch for Python
    
    * Update package.json

--- src/edge/edgeManager.ts
+++ src/edge/edgeManager.ts
@@ -324,7 +324,7 @@ export class EdgeManager {
                 break;
             case Constants.LANGUAGE_PYTHON:
                 const gitHubSource = "https://github.com/Azure/cookiecutter-azure-iot-edge-module";
-                const branch = "develop"; // TODO: Change back to master before release
+                const branch = "master";
                 await Executor.executeCMD(outputChannel,
                     "cookiecutter",
                     { cwd: `${parent}`, shell: true },

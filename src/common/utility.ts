393009f014cba2da182ab95112c30e50822fe54e
Ray Fang <fangzh@microsoft.com>
2018-11-19 13:32:53 +0800

    Fix a typo

--- src/common/utility.ts
+++ src/common/utility.ts
@@ -501,7 +501,7 @@ export class Utility {
         const re = new RegExp(`(.|[\r\n]){1,${Constants.TwinValueMaxSize}}`, "g");
         const options = optionStr.match(re);
         if (options.length > Constants.TwinValueMaxChunks) {
-            throw new Error(`Size of createOptions of ${settings.image} is too big. The maxium size of createOptions is 4K`);
+            throw new Error(`Size of createOptions of ${settings.image} is too big. The maximum size of createOptions is 4K`);
         }
         options.map((value, index) => {
             if (index === 0) {

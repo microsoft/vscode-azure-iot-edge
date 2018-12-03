1342e764eece37817e421d77f47b8dae5a593af0
Ray Fang <fangzh@microsoft.com>
2018-12-03 20:00:44 +0800

    Fix issue packing ms-rest and AI

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,15 @@ const config = {
         extensions: ['.ts', '.js', '.json']
     },
     module: {
+        // require
+        unknownContextRegExp: /$^/,
+        unknownContextCritical: false,
+        // require(expr)
+        exprContextRegExp: /$^/,
+        exprContextCritical: false,
+        // require("prefix" + expr + "suffix")
+        wrappedContextRegExp: /$^/,
+        wrappedContextCritical: false,
         rules: [{
             test: /\.ts$/,
             exclude: /node_modules/,

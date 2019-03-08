32d9586f050d9f10be92a59cfcc2a25b12f1e511
formulahendry <formulahendry@gmail.com>
2019-03-08 16:27:59 +0800

    Code cleanup

--- assets/marketplace/main.js
+++ assets/marketplace/main.js
@@ -14,12 +14,11 @@ const app = new Vue({
         selectedTag: "",
         moduleName: '',
         modules: [],
-        endpoint: document.getElementById('app').getAttribute('data-endpoint').includes("{{") ? "http://localhost:54050" : document.getElementById('app').getAttribute('data-endpoint'),
+        endpoint: document.getElementById('app').getAttribute('data-endpoint'),
         errorMessage: "",
     },
     created: async function () {
         this.modules = await this.getModules();
-        console.log(this.modules )
     },
     methods: {
         getModules: async function () {
@@ -45,7 +44,6 @@ const app = new Vue({
             return data;
         },
         showModule: async function (module) {
-            // this.selectedModule.id = '';
             this.errorMessage = "";
             const metadata = await this.getModuleMetadata(module);
             this.selectedModule = Object.assign({}, module);
@@ -99,31 +97,3 @@ const app = new Vue({
         }
     }
 });
-
-async function getData(type, data) {
-
-    data.type = type;
-    // vscode.postMessage(data);
-    return new Promise((resolve) => {
-        window.addEventListener('message', event => {
-
-            const message = event.data; // The JSON data our extension sent
-
-            resolve(event);
-        });
-    })
-}
-
-async function f2() {
-    let data = await getData('aa', {
-        'aa': 1
-    });
-    console.log(111)
-    console.log(data);
-    let data2 = await getData('bb', {
-        'vv': 1
-    });
-    console.log(data2);
-    console.log(222)
-}
-f2();

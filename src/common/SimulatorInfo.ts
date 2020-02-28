6e92c3e2195a41941256e6aa2e5a5efba4c24afe
rentu <rentu@microsoft.com>
2020-02-28 14:11:53 +0800

    Add copyright declaration

--- src/common/SimulatorInfo.ts
+++ src/common/SimulatorInfo.ts
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 export class SimulatorInfo {
     public version: string;
     public standaloneDownloadUrl: string;

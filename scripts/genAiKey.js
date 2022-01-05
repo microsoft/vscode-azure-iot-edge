545a1ca0a6646ebc8b1a5287e8a6527d5afdb81f
Vikas <84580678+vikas0212git@users.noreply.github.com>
2022-01-05 15:21:34 -0800

    updated

--- scripts/genAiKey.js
+++ scripts/genAiKey.js
@@ -5,9 +5,9 @@ if (process.env.BUILD_SOURCEBRANCH) {
     const packageJson = JSON.parse(fs.readFileSync("package.json"));
     if (ISPROD) {
         packageJson.aiKey = process.env["PROD_AIKEY"];
+        console.log("Updated Prod AiKey");
     } else {
         packageJson.aiKey = process.env["INT_AIKEY"] || packageJson.aiKey;
-        console.log("Updated INT AiKey");
     }
     fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2) + "\n");
     console.log("Updated AiKey");

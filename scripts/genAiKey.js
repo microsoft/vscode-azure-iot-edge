ebb277d74999909b1360260f82706d54ca9a444a
Vikas <84580678+vikas0212git@users.noreply.github.com>
2022-01-06 09:43:31 -0800

    User/vikas/migration (#623)
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Updated the Azure pipelines file.
    
    * updated file
    
    * updated file
    
    * Updated file
    
    * Updated file
    
    * updated gh release
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * updated azure pipelines file
    
    * updated azurepipelines file
    
    * updated file
    
    * updated azure pipelines.
    
    * updated file
    
    * updated file
    
    * updated yml file
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * updated
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * updated
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * updated file
    
    * updated
    
    * updated file
    
    * updated file
    
    * updated
    
    * updated file
    
    * updated
    
    * update
    
    * update
    
    * updated
    
    * updated
    
    * updated
    
    * updated file

--- scripts/genAiKey.js
+++ scripts/genAiKey.js
@@ -1,15 +1,16 @@
 const fs = require("fs");
 
-if (process.env.GITHUB_TAG) {
-    const ISPROD = new RegExp(process.env.ISPRODTAG).test(process.env.GITHUB_TAG || "");
+if (process.env.BUILD_SOURCEBRANCH) {
+    const ISPROD = new RegExp(process.env.ISPRODTAG).test(process.env.BUILD_SOURCEBRANCH || "");
     const packageJson = JSON.parse(fs.readFileSync("package.json"));
     if (ISPROD) {
         packageJson.aiKey = process.env["PROD_AIKEY"];
+        console.log("Updated Prod AiKey");
     } else {
         packageJson.aiKey = process.env["INT_AIKEY"] || packageJson.aiKey;
+        console.log("Updated INT AiKey");
     }
     fs.writeFileSync("package.json", JSON.stringify(packageJson, null, 2) + "\n");
-    console.log("Updated AiKey");
 } else {
     console.log("Skipping genAiKey");
 }
